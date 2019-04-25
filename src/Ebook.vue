ee2b20979cf1329d4e3cb1886266747b0d9342a5
memory-you <869529706@qq.com>
2019-04-25 22:58:35 +0800

    Thur First Commit

---
+++ src/Ebook.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div id="read"></div>
+  </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
+global.ePub = Epub
+export default {
+  methods: {
+    // 电子书的解析与渲染
+    showEpub() {
+      //  生成Book
+      this.book = new Epub(DOWNLOAD_URL)
+      //  生成Rendition,通过Book.renderTO
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      //  通过Renditon.display渲染电子书
+      this.rendition.display()
+    }
+  },
+  mounted() {
+    this.showEpub()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
